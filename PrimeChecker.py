e3bfe228a54dbbe05b25621b758a6217add7be55
Zabil Sabri Muhammad <79674049+zabilsabri@users.noreply.github.com>
2021-06-10 06:09:44 +0800

    Update PrimeChecker.py

--- PrimeChecker.py
+++ PrimeChecker.py
@@ -1,20 +1,11 @@
-jawaban = []
+answer = []
 
-
-def prime_check(angka=int(input("MASUKKAN ANGKA: "))):
-    for i in range(1, angka + 1):
-        if angka % i == 0:
-            jawaban.append(i)
-
-    if angka == 1 or angka == 0:
-        print("PRIME")
-
-
-    elif jawaban[0] == 1 and jawaban[1] == angka:
-        print("PRIME")
-
-    else:
-        print("Not PRIME")
-
-
-prime_check()
+for i in range(1, number=int(input("PUT YOUR NUMBER IN: "))):
+    if number % i == 0:
+        answer.append(i)
+        
+if len(answer) == 1 or len(answer) == 2:
+    print("PRIME")
+    
+else:
+    print("NOT PRIME")
